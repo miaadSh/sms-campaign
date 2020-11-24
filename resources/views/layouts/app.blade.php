5508bec41e149ed8629b3c7721654f51d176c4fb
miaad shahrokhi <miaad.66@gmail.com>
2020-11-24 17:32:31 +0330

    add tailwind and livewire to sms-campaign project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css', 'vendor/zoomila/sms-campaign') }}">
     <title>Sms Camapaign</title>
     @livewireStyles
 </head>
@@ -13,5 +14,7 @@
     @yield('content')
 
     @livewireScripts
+
+<script src="{{ mix('js/app.js', 'vendor/zoomila/sms-campaign') }}"></script>
 </body>
 </html>
--- src/SmsCampaignServiceProvider.php
+++ src/SmsCampaignServiceProvider.php
@@ -11,17 +11,48 @@ use SmsCampaign\Http\Livewire\Sms\Template;
 class SmsCampaignServiceProvider extends ServiceProvider{
 
     public function register(){
-
     }
 
     public function boot(){
+
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views','sms');
         $this->loadViewsFrom(__DIR__.'/../resources/views/livewire','smsComponent');
 
+        $this->configurePublishing();
+
         Livewire::component('smsComponent::campaign', Campaign::class);
         Livewire::component('smsComponent::group', Group::class);
         Livewire::component('smsComponent::template', Template::class);
+    }
+
+
+    /**
+     * Configure publishing for the package.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../public' => public_path('vendor/zoomila/sms-campaign'),
+            ], 'zoomila-assets');
+
+            $this->publishes([
+                __DIR__.'/../config/sms-campaign.php' => config_path('sms-campaign.php'),
+            ], 'zoomila-config');
+
+            $this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/zoomila'),
+            ], 'zoomila-lang');
+
+            /* $this->publishes([
+                __DIR__.'/../resources/stubs/providers/CanvasServiceProvider.stub' => app_path(
+                    'Providers/CanvasServiceProvider.php'
+                ),
+            ], 'zoomila-provider'); */
+        }
     }
 
 }
